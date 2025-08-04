3ea8c0abc1351b43a8e600c61c8c0079c5410550
Jasper Smet <josbeir@gmail.com>
2025-08-04 09:51:06 +0200

    Clean up code-sniffing violations in baked migrations
    This cleans up baked migrations to prevent:
    
    "SlevomatCodingStandard.Commenting.DocCommentSpacing.IncorrectLinesCountBetweenDescriptionAndAnnotations"
    (Expected 1 line between description and annotations, found 0.) complaints by codesniffing rules.

--- tests/comparisons/Migration/testCreatePrimaryKeyUuid.php
+++ tests/comparisons/Migration/testCreatePrimaryKeyUuid.php
@@ -12,6 +12,7 @@ class CreateUsers extends BaseMigration
      *
      * More information on this method is available here:
      * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
      * @return void
      */
     public function change(): void
--- tests/comparisons/Migration/test_snapshot_plugin_blog.php
+++ tests/comparisons/Migration/test_snapshot_plugin_blog.php
@@ -10,6 +10,7 @@ class TestSnapshotPluginBlog extends BaseMigration
      *
      * More information on this method is available here:
      * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     *
      * @return void
      */
     public function up(): void
@@ -134,6 +135,7 @@ class TestSnapshotPluginBlog extends BaseMigration
      *
      * More information on this method is available here:
      * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     *
      * @return void
      */
     public function down(): void
